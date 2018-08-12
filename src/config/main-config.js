e5982f33d8b328a0852c3eabcb0fca8a2ba99c0a
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-12 15:13:54 +0900

    finished CRUD for topics

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const path = require('path');
+const bodyParser = require('body-parser');
 const viewsFolder = path.join(__dirname, '..', 'views');
 
 // Set view location for template engine and set where to find static assets
@@ -7,6 +8,7 @@ module.exports = {
 	init(app, express) {
 		app.set('views', viewsFolder);
 		app.set('view engine', 'ejs');
+		app.use(bodyParser.urlencoded({ extended: true }));
 		app.use(express.static(path.join(__dirname, '..', 'assets')));
 	}
 };
