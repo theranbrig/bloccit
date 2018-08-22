24b6176f52f8f055f6979c2fabd68f0df3f72a29
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-22 22:41:35 +0900

    added comment tests for admins and members

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -1,8 +1,6 @@
-// #1
 const request = require('request');
 const server = require('../../src/server');
 const base = 'http://localhost:3000/topics/';
-
 const sequelize = require('../../src/db/models/index').sequelize;
 const Topic = require('../../src/db/models').Topic;
 const Post = require('../../src/db/models').Post;
@@ -150,7 +148,6 @@ describe('routes : comments', () => {
 			);
 		});
 
-		// #2
 		describe('POST /topics/:topicId/posts/:postId/comments/create', () => {
 			it('should create a new comment and redirect', done => {
 				const options = {
@@ -177,6 +174,113 @@ describe('routes : comments', () => {
 
 		describe('POST /topics/:topicId/posts/:postId/comments/:id/destroy', () => {
 			it('should delete the comment with the associated ID', done => {
+				Comment.all().then(comments => {
+					const commentCountBeforeDelete = comments.length;
+					expect(commentCountBeforeDelete).toBe(1);
+					request.post(
+						`${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+						(err, res, body) => {
+							expect(res.statusCode).toBe(302);
+							Comment.all().then(comments => {
+								expect(err).toBeNull();
+								expect(comments.length).toBe(commentCountBeforeDelete - 1);
+								done();
+							});
+						}
+					);
+				});
+			});
+		});
+	});
+
+	///////////////////////////////////////////////////////////////////////////
+	// Assignment: Signed in user should not delete another user's information
+
+	describe('Signed in users should not delete another comment', () => {
+		describe('POST /topics/:topicId/posts/:postId/comments/create', () => {
+			beforeEach(done => {
+				request.get(
+					{
+						url: 'http://localhost:3000/auth/fake',
+						form: {
+							role: 'member',
+							userId: 873648234
+						}
+					},
+					(err, res, body) => {
+						done();
+					}
+				);
+			});
+			it('should not delete another members comment', done => {
+				Comment.all().then(comments => {
+					const commentCountBeforeDelete = comments.length;
+					expect(commentCountBeforeDelete).toBe(1);
+					request.post(
+						`${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+						(err, res, body) => {
+							expect(res.statusCode).toBe(401);
+							Comment.all().then(comments => {
+								expect(err).toBeNull();
+								expect(comments.length).toBe(commentCountBeforeDelete);
+								done();
+							});
+						}
+					);
+				});
+			});
+		});
+	});
+
+	////////////////////////////////////////////////////////
+	///  ADMIN USERS
+	////////////////////////////////////////////////////////
+
+	describe('admin user performing CRUD actions for on another user Comment', () => {
+		describe('POST /topics/:topicId/posts/:postId/comments/create', () => {
+			beforeEach(done => {
+				request.get(
+					{
+						url: 'http://localhost:3000/auth/fake',
+						form: {
+							role: 'member',
+							userId: this.user.id
+						}
+					},
+					(err, res, body) => {
+						done();
+					}
+				);
+			});
+		});
+
+		///////////////////////////////////////////////////////////////////////////
+		// Assignment: Admin user should be able to delete content
+
+		describe('POST /topics/:topicId/posts/:postId/comments/:id/destroy', () => {
+			beforeEach(done => {
+				User.create({
+					email: 'admin@example.com',
+					password: '123456',
+					role: 'admin'
+				}).then(user => {
+					request.get(
+						{
+							url: 'http://localhost:3000/auth/fake',
+							form: {
+								role: user.role,
+								userId: user.id,
+								email: user.email
+							}
+						},
+						(err, res, body) => {
+							done();
+						}
+					);
+				});
+			});
+
+			it('should delete another members comment', done => {
 				Comment.all().then(comments => {
 					const commentCountBeforeDelete = comments.length;
 					expect(commentCountBeforeDelete).toBe(1);
