9221db83a92f0f515d4ad4bcd31e51dc0c4fd981
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-08-23 00:07:02 +0900

    added voting resource

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -7,6 +7,7 @@ module.exports = {
 		const postRoutes = require('../routes/posts');
 		const userRoutes = require('../routes/users');
 		const commentRoutes = require('../routes/comments');
+		const voteRoutes = require('../routes/votes');
 		if (process.env.NODE_ENV === 'test') {
 			const mockAuth = require('../../spec/support/mock-auth.js');
 			mockAuth.fakeIt(app);
@@ -16,5 +17,6 @@ module.exports = {
 		app.use(postRoutes);
 		app.use(userRoutes);
 		app.use(commentRoutes);
+		app.use(voteRoutes);
 	}
 };
--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -2,12 +2,20 @@ const Post = require('./models').Post;
 const Topic = require('./models').Topic;
 const Comment = require('./models').Comment;
 const User = require('./models').User;
+const Vote = require('./models').Vote;
 const Authorizer = require('../policies/post');
 
 module.exports = {
 	getPost(id, callback) {
 		return Post.findById(id, {
-			include: [{ model: Comment, as: 'comments', include: [{ model: User }] }]
+			include: [
+				{
+					model: Comment,
+					as: 'comments',
+					include: [{ model: User }]
+				},
+				{ model: Vote, as: 'votes' }
+			]
 		})
 			.then(post => {
 				callback(null, post);
